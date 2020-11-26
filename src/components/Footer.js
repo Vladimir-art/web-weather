d5660cc948dce1785eda756da3545431f54bfdec
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-26 23:25:44 +0300

    footer markup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import github from '../images/github-icon.svg';
 
 function Footer() {
-  return(
-    <>
-    </>
+  return (
+    <footer className="footer">
+      <address className="footer__author">&copy; 2020 Vladimir Ermolaev</address>
+      <a className="footer__link" href="https://github.com/Vladimir-art/web-weather" target="_blank" rel="noreferrer">
+        <img className="footer__icon" alt="GitHub" src={github} />
+      </a>
+    </footer>
   )
 }
 
