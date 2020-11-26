b43d8f77adfda94da702e6b9d6c267fd092a4412
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-26 20:59:21 +0300

    Add header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <h3 className="header__title">WebWeather</h3>
+      <h3 className="header__title">Web Weather</h3>
     </header>
   )
 }
