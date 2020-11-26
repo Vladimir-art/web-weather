fddcd8c43a192603ec6732fd3207abb141b12510
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-26 22:21:54 +0300

    SearchingWeather styles

--- src/components/SearchingWeather.js
+++ src/components/SearchingWeather.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function SearchingWeather() {
   return (
-    <>
-    </>
+    <section className="serching-weather">
+      <h1 className="serching-weather__title">
+        What's the weather like today?
+      </h1>
+      <form className="serching-weather__form">
+        <input className="serching-weather__input" type="text" placeholder="Enter the city" required />
+        <button className="serching-weather__search" type="submit"></button>
+      </form>
+    </section>
   )
 }
 
