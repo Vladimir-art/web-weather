541e91392424b6facbab0976836d98509a1f51e3
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-26 22:11:17 +0300

    Create SearchingWeather & add to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SearchingWeather from './SearchingWeather';
 
 function Main() {
   return(
     <main className="main">
-
+      <SearchingWeather />
     </main>
   )
 }
---
+++ src/components/SearchingWeather.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SearchingWeather() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default SearchingWeather;
