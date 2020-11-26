df4f08dcadf08d92dd75a5052d0575f4206293b8
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-11-26 23:54:27 +0300

    update some styles

--- src/components/SearchingWeather.js
+++ src/components/SearchingWeather.js
@@ -7,9 +7,10 @@ function SearchingWeather() {
         What's the weather like today?
       </h1>
       <form className="serching-weather__form">
-        <input className="serching-weather__input" type="text" placeholder="Enter the city" required />
+        <input className="serching-weather__input" type="text" placeholder="Enter the city" id="weather" required />
         <button className="serching-weather__search" type="submit">Search</button>
       </form>
+      <span className="serching-weather__error" id="weather">Sorry, I can't find this city. Try again</span>
     </section>
   )
 }
